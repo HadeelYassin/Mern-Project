66f85e5249279daec1df840c6d41b60f6da69067
Shatha96 <shathabast62@gmail.com>
2021-02-20 19:14:39 +0200

    edit1

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Card } from '@material-ui/core';
-// import {FacebookIcon} from '@material-ui/icons/Facebook';
-// import TwitterIcon from '@material-ui/icons/Twitter';
-// import InstagramIcon from '@material-ui/icons/Instagram';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import InstagramIcon from '@material-ui/icons/Instagram';
 
 
 const Footer = () => {
@@ -11,6 +11,9 @@ const Footer = () => {
         <div>
             <Card style={{backgroundColor:"#21262d", color:'whitesmoke', fontSize:'12px'}} elevation={3}>
                 <p>Movie Time © 2021</p>
+                <FacebookIcon color="primary"/>
+                <TwitterIcon/>
+                <InstagramIcon olor="secondary"/>
             </Card>
         </div>
     )
