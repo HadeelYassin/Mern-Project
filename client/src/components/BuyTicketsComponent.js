80728d910ef8230da166db877209df4e978745b3
Hadeel Yassin <hadeelyassin1997@gmail.com>
2021-02-20 11:26:15 +0200

    hadeel's first commit

---
+++ client/src/components/BuyTicketsComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BuyTicketsComponent = () => {
+    return (
+        <div>
+            <p>hello</p>
+        </div>
+    )
+}
+
+export default BuyTicketsComponent
