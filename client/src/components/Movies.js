994652dd59cb5548fd5966a9391a19b0d9f719b0
MaisDroubi <mais.droubi@gmail.com>
2021-02-22 14:14:27 +0200

    style edited

--- client/src/components/Movies.js
+++ client/src/components/Movies.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import '../styles/movies.css';
-import { Button } from '@material-ui/core';
+// import { Button } from '@material-ui/core';
 
 
 
@@ -15,17 +15,19 @@ const Movies = ({ poster_path, title, vote_average, overview, video, release_dat
         <h4>{title}</h4>
       </div> */}
       <img src={IMGPATH + poster_path} alt={title} />
-      <div class="movie-date">
-        <h4>{"FRIDAY, FEBRUARY 12"}</h4>
-        <h4>{"10:00 am "}</h4>
+      <div className="movie-date">
+        <h4>{"FRIDAY, FEBRUARY 12"}<br/>
+        {"10:00 am "}</h4>
       </div>
       <div className="overview">
-        {/* {video} */}
         <div>
           <iframe src="https://www.youtube.com/embed/6ZfuNTqbHE8?autoplay=1&mute=1" height="100%" width="100%" title="W3Schools Free Online Web Tutorials"></iframe>
+          <h4>{"category"}</h4>
           <p>{overview}</p>
         </div>
-        <button style={{backgroundColor:"#f9ff4b", border:'none'}}>Book A Ticket </button>
+        <h6>{"There is a ticket !!"}</h6>
+
+        <button className="buy">Book Now</button>
         {/* <Button variant="contained" color="primary" disableElevation>
           Buy Now!!
         </Button> */}
